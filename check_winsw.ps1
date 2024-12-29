25f9dfd75b6fd557b30907550f63a652cdb5ee07
Tommi Muona <43796235+muonato@users.noreply.github.com>
2024-12-29 11:23:34 +0200

    Check installed software version

--- check_winsw.ps1
+++ check_winsw.ps1
@@ -1,10 +1,10 @@
-# muonato/check_winsw.ps1 @ GitHub (28-DEC-2024)
+# muonato/check_winsw.ps1 @ GitHub (29-DEC-2024)
 #
-# Reports installed software by reading 'DisplayName' and 'DisplayVersion'
-# keys under the hive 'HKEY_LOCAL_MACHINE\SOFTWARE\' in Windows registry
+# Reports software version by matching name in argument string to
+# Windows registry key under the hive 'HKEY_LOCAL_MACHINE\SOFTWARE\'
 #
 # Usage:
-#       PS> check_winsw.ps1 "[LF][,<application registry key>] ..."
+#       PS> check_winsw.ps1 "[LF][,<application>] ..."
 #
 # Parameters:
 #       1: String with application names separated by comma
@@ -13,25 +13,29 @@
 #       formats output with 'LF' for line feed
 #
 # Examples:
-#       Check all installed software, default CSV output
-#       PS> check_winsw.ps1 "*"
+#       Check OS version only
+#       PS> check_winsw.ps1
 #
 #       Opsview / Nagios host monitoring syntax
-#       check_nrpe -H $HOSTADDRESS$ -c check_winsw -a "*"
+#       check_nrpe -H $HOSTADDRESS$ -c check_winsw -a "Microsoft"
 #
 #       Check two applications, line feed output
-#       PS> check_winsw.ps1 "LF,Microsoft Edge,VLC media player"
+#       PS> check_winsw.ps1 "LF,SQL,VLC"
 
-function Get-WinSW([string]$regpath,[string]$format,[string]$product="DisplayName",[string]$version="DisplayVersion") {
-    # Returns product and version keys in defined registry path
+function Get-WinSW([string]$regpath,[string]$software,[string]$format,[string]$product="DisplayName",[string]$version="DisplayVersion") {
+    # Returns matching product and version keys in defined registry path
+    
     $winsw = ""
     Get-ItemProperty $regpath -ErrorAction SilentlyContinue | Select-Object $product, $version | ForEach-Object {
-        if (-not ([string]::IsNullOrEmpty($_.$product))) {
+        $match = "$_.$product" | Select-String $software
+
+        if (-not ([string]::IsNullOrEmpty($match))) {
             $winsw = -join ($winsw, $format -f $_.$product, $_.$version)
         }
     }
     $winsw
 }
+
 $info = ""
 $fmtc = ", "
 $arry = @()
@@ -41,25 +45,27 @@ $arry = @()
 if ($args.count -eq 1) {
     $arry = $args[0].Split(",")
 }
+
 # First param in CSV argument string to 
 # format output with 'LF' for line feed
 if ($arry[0] -eq "LF") {
-    $apps = $arry | Where-Object { $_ -ne "LF" }
+    $apps = $arry | Where-Object { $_ –ne "LF" }
     $fmtc = "`r`n"
 } else {
     $apps = $arry
 }
-# Loop registry hives defined in the CSV argument string by application name ('*' = all)
-foreach ($name in $apps) {
-    $hive = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\$name"
-    $info = -join ($info,(Get-WinSW -regpath $hive -format "{0} ({1}) [Win32]$fmtc"))
 
-    $hive = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\$name"
-    $info = -join ($info,(Get-WinSW -regpath $hive -format "{0} ({1})$fmtc"))
+# Loop registry hives defined in the CSV argument string ('*' = all)
+foreach ($name in $apps) {
+    $hive = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    $info = -join ($info,(Get-WinSW -regpath $hive -software $name -format "{0} ({1}) [Win32]$fmtc"))
+
+    $hive = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+    $info = -join ($info,(Get-WinSW -regpath $hive -software $name -format "{0} ({1})$fmtc"))
 }
 # Windows version information to report as last value
 $hive = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion"
-$info = -join ($info,(Get-WinSW -regpath $hive -format "{0} ({1})" -product "ProductName" -version "DisplayVersion"))
+$info = -join ($info,(Get-WinSW -regpath $hive -software "Windows" -format "{0} (Build {1})" -product "ProductName" -version "CurrentBuild"))
 
 Write-Host $info
 exit 0
